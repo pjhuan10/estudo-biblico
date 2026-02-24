d357bcb3e2e539756474ad59e9e795bc1e3a246b
Jhuan Pablo <jhuanpablo@MacBook-Air-de-Jhuan.local>
2026-02-24 00:28:22 -0300

    fix: revalidate literal value

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { getVerseOfDay } from "@/lib/verseOfDay";
 
-export const revalidate = 60 * 60 * 24; 
+export const revalidate = 86400;
 
 export default function HomePage() {
   const verse = getVerseOfDay();
